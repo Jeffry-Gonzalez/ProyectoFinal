6b74eb3ca00205c074a9942235d913475bb30488
Jeffry-Gonzalez <66580178+Jeffry-Gonzalez@users.noreply.github.com>
2020-06-07 22:44:02 -0600

    Add files via upload

---
+++ Proyecto Final/Nodo.java
@@ -0,0 +1,11 @@
+package gt.edu.umg.progra1.ejemplo;
+
+public class Nodo {
+	int elemento;
+	Nodo siguiente;
+
+	Nodo(int x) {
+		elemento = x;
+		siguiente = null;
+	}
+}
---
+++ Proyecto Final/Pila.java
@@ -0,0 +1,56 @@
+package gt.edu.umg.progra1.ejemplo;
+
+public class Pila {
+	private Nodo cima;
+	public Pila() {
+		cima = null;
+	}
+
+	public boolean pilaVacia() {
+		if (cima == null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void insertar(int elemento) {
+		Nodo nuevo;
+		nuevo = new Nodo(elemento);
+		nuevo.siguiente = cima;
+		cima = nuevo;
+		System.out.println("Dato agregado");
+	}
+
+	public int quitar() {
+		if (pilaVacia()) {
+			return -1;
+		}
+		int aux = cima.elemento;
+		cima = cima.siguiente;
+		System.out.println("Dato eliminado: " + aux);
+		return aux;
+	}
+
+	public void vaciar() {
+		Nodo t;
+		while (!pilaVacia()) {
+			t = cima;
+			cima = cima.siguiente;
+			t.siguiente = null;
+		}
+	}
+	
+	public void listar() {
+		if (pilaVacia()) {
+			System.out.println("La pila esta vacia");
+		} else {
+			Nodo t;
+			t = cima;			
+			while (t != null) {
+				System.out.println("Dato: " + t.elemento);
+				t = t.siguiente;
+			}
+		}
+	}
+}
